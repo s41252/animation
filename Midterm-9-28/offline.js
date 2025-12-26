454d289cd10b3d45bb9916cd4057359c8ef6d3ce
s41252 <s41252@bj.ac.th>
2025-12-26 20:23:23 +0700

    Add files via upload

---
+++ Midterm-9-28/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1766755102,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground2.png",
+		"images/player-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/tiledbackground.png",
+		"images/cloud-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
